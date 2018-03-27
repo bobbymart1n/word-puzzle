61c8e712d10a6725fe4ae8e338dad4ea9a031b4f
Bobby Martin and William Chu <bobby@>
2018-03-27 10:41:38 -0700

    Add second answer array, update loop logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,22 @@ $(function() {
     event.preventDefault();
     var userInput = $("#userString").val().trim().toLowerCase();
     var userArray = userInput.split("");
+    var userAnswer = userArray.slice();
+    // userAnswer.forEach(function(letter) {
+    //   if(letter === "-") {
+    //     letter = " ";
+    //   }
+    // });
+    // console.log(userAnswer);
+    // userAnswer.join("");
+    // console.log(userAnswer, "HERE");
     for(var i = 0; i < userArray.length; i++) {
       if(userArray[i] === "-") {
         userArray[i] = " ";
+        userAnswer[i] = " ";
       }
-      console.log(userArray);
+      // console.log(userArray, "Here");
+      // console.log(userAnswer);
       for (var j = 0; j < vowels.length; j++) {
         if(userArray[i] === vowels[j]) {
           userArray[i] = "-";
@@ -20,8 +31,11 @@ $(function() {
     // Remove form input and show buttons
     $("#stringInput").detach();
     $(".btn-group").show();
-    var ouputString = userArray.join("");
-    $("#output").append("<h3>Here is your clue, time to guess:</h3><br>" + ouputString);
+    var outputString = userArray.join("");
+    var outputAnswer = userAnswer.join("");
+    console.log(outputString);
+    console.log(outputAnswer);
+    $("#output").append("<h3>Here is your clue, time to guess:</h3><br>" + outputString + outputAnswer);
 
     // Guessing letters
     var guess = "";
