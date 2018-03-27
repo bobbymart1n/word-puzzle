12ae5de923c6e5b5294ed5aeb657f98c78640d10
Bobby Martin and William Chu <william@>
2018-03-27 08:37:39 -0700

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
 $(function() {
   $("#userInput").submit(function(event){
     event.preventDefault();
-
-    var countTo = parseInt($("#countTo").val());
-    var countBy = parseInt($("#countBy").val());
-
-    if (countTo <= countBy) {
-      alert("Please enter a count by number that is less than the count to number");
-    }
-    else if (countBy <= 0 || countTo <=0) {
-      alert("Please enter a positive count by and count to number")
-    }
-    else {
-      for (var i = countBy; i <= countTo; i += countBy) {
-        $("#output").append(", " + i);
-      };
-    }
-
-
+    
   });
 });
