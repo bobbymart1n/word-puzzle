4669ceab9e8f01dfd13a3fa15fc4c2d131812592
Bobby Martin and William Chu <william@>
2018-03-27 09:27:41 -0700

    Add vowel buttons, add logic to form

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@ $(function() {
   var vowels = ["a", "e", "i", "o", "u", "y"];
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#userString").val().trim();
+    var userInput = $("#userString").val().trim().toLowerCase();
     var userArray = userInput.split("");
-    console.log(userArray);
+    for(var i = 0; i < userArray.length; i++) {
+      for (var j = 0; j < vowels.length; j++) {
+        if(userArray[i] === vowels[j]) {
+          userArray[i] = "-";
+        }
+      }
+    };
+    $("#stringInput").detach();
+    $(".btn-group").show();
+    var ouputString = userArray.join("");
+    $("#output").append("<h3>Here is your clue, time to guess:</h3><br>" + ouputString);
   });
 });
-// Grab value userInput
-// Split userInput
- // loop through that split sentence(array)
- // replace all vowels with "-"
- // join new array
- //
+
+// grab button
