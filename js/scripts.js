f1255e16795d404eecc7513721a4d32c43ca8e60
Bobby Martin and William Chu <william@>
2018-03-27 08:57:16 -0700

    Add user input field and js to grab value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(function() {
-  $("#userInput").submit(function(event){
+  var vowels = ["a", "e", "i", "o", "u", "y"];
+  $("#userInput").submit(function(event) {
     event.preventDefault();
-    
+    var userInput = $("#userString").val().trim();
+    var userArray = userInput.split("");
+    console.log(userArray);
   });
 });
+// Grab value userInput
+// Split userInput
+ // loop through that split sentence(array)
+ // replace all vowels with "-"
+ // join new array
+ //
