bdb6555b39553b520922a13e210b7be2767ccf8b
Bobby Martin and William Chu <bobby@>
2018-03-27 13:40:52 -0700

    add vowel validation check for user input, lose condition

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,39 @@ $(function() {
     // clone user array
     var userAnswer = userArray.slice();
     // loop through user array to remove user entered "-", take same action on clone (this only works because they are clones)
-    for(var i = 0; i < userArray.length; i++) {
-      if(userArray[i] === "-") {
-        userArray[i] = " ";
-        userAnswer[i] = " ";
-      }
-      // loop through vowels to replace with "-"
-      for (var j = 0; j < vowels.length; j++) {
-        if(userArray[i] === vowels[j]) {
-          userArray[i] = "-";
+    var vowelCheck = 0;
+    // If the user doesn't enter any vowels send alert
+    userArray.forEach(function(check) {
+      for (var v = 0; v < vowels.length; v++) {
+        if(check === vowels[v]) {
+          vowelCheck += 1;
         }
       }
-    };
-    // Remove form input and show buttons
-    $("#stringInput").detach();
-    $(".btn-group").show();
-    var outputString = userArray.join("");
-    var outputAnswer = userAnswer.join("");
-    // Show clue with dashed string
-    $("#output").append("<h3>Here is your clue, time to guess:</h3><br>" + outputString);
+    });
+    if (vowelCheck >= 1) {
+      for(var i = 0; i < userArray.length; i++) {
+        if(userArray[i] === "-") {
+          userArray[i] = " ";
+          userAnswer[i] = " ";
+        }
+        // loop through vowels to replace with "-"
+        for (var j = 0; j < vowels.length; j++) {
+          if(userArray[i] === vowels[j]) {
+            userArray[i] = "-";
+          }
+        }
+      };
+      // Remove form input and show buttons
+      $("#stringInput").detach();
+      $(".btn-group").show();
+      var outputString = userArray.join("");
+      var outputAnswer = userAnswer.join("");
+      // Show clue with dashed string
+      $("#output").append("<h3>Here is your clue, time to guess:</h3><br>" + outputString);
+    } else {
+      $(".vowel-check").show();
+      $("#userString").addClass("vowel-err");
+    }
 
     // Set guess as empty string
     var guess = "";
@@ -39,23 +53,28 @@ $(function() {
       $(this).remove();
       $("#output").empty();
       // set counter var
-      var count = 0;
+      var winCheck = 0;
+      var lossCheck = 0;
       // loop through userArray and compare to guess value
       for(var i = 0; i < userArray.length; i++) {
         if(userAnswer[i] === guess) {
           userArray[i] = guess;
+          lossCheck += 1;
           outputString = userArray.join("");
         }
       }
       // loop through userArray to compare count to remainder of dashes
       userArray.forEach(function(cnt) {
         if(cnt === "-") {
-          count += 1;
+          winCheck += 1;
         }
       });
       // Win condition, if no more dashes remaining in outputString
-      if(count === 0) {
-        $("#output").append("<h1>You've WON!!</h1><br>" + outputString);
+      if(winCheck === 0) {
+        $("#output").append("<h1>You've WON!!!</h1><br>" + outputString);
+      } else if (lossCheck === 0) {
+        $("#output").append("<h1>You Lost</h1><br>" + outputString + "<br><button href='index.html' class='btn btn-danger'>Try Again</button>");
+        $("#userInput")[0].reset();
       } else {
         $("#output").append("<h3>Here is your updated clue, time to guess:</h3><br>" + outputString);
       }
